49d85a290244e39ad1b9ee4bab6807f45ccb9cb5
Valk <bznand@gmail.com>
2024-05-31 13:14:54 +0200

    checkout success responsive

--- src/pages/checkout/Success/Success.tsx
+++ src/pages/checkout/Success/Success.tsx
@@ -1,3 +1,4 @@
+import css from "./Success.module.css"
 
 export default function Success() {
 
@@ -6,8 +7,8 @@ export default function Success() {
     return (
         <div className="text-center">
             <h2 className="mb-3 display-5">Thank you for purchasing from Retrowares!</h2>
-            <div className="mb-5">
-                <img src="/images/thanks.png" />
+            <div className="mb-5 mw-100">
+                <img src="/images/thanks.png" className={css.SuccessImage}/>
             </div>
             <p>Your order is scheduled for delivery in 3-5 business days.
                 Order status can be viewed any time by using the order code through our tracking services.</p>
