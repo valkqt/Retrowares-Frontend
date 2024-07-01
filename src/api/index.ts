b79d75630ffe93d19bafb7e72599e5a144b9df53
Valk <bznand@gmail.com>
2024-07-01 16:53:34 +0200

    same as last

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const instance = axios.create({
-    baseURL: "http://retrowares.store/api/",
+    baseURL: "retrowares.store/api/",
     // baseURL: "https://localhost:7131/",
     timeout: 5000,
     headers: {"Content-Type": "application/json"}
